09309f6b43508e2680aa9716ef73ba4720b0c3c8
stratocagm <stratocagm@gmail.com>
2019-06-12 22:25:12 -0500

    Nuevo

---
+++ src/HelperCursos.js
@@ -0,0 +1,75 @@
+const hbs = require('hbs');
+const fs = require('fs');
+
+listaCursos = [];
+
+const listar = () => {
+    try{
+        listaCursos = require('../ListadoCursos.json');
+    }catch(error){
+        listaCursos = [];
+    }
+}
+
+hbs.registerHelper('CrearCurso', (id, nombre, descripcion, valor, modalidad, error) => {
+    listar();
+    if(error != undefined){
+        console.log(error.Array);
+        for(let i=0; i < error.length; i++){
+            console.log(error[i]);
+        }
+        return error;
+    }
+    let cur = {
+        id : id,
+        nombre : nombre,
+        descripcion : descripcion,
+        valor : valor,
+        modalidad: modalidad,
+        estado: 'Disponible'
+    };
+    let duplicado = listaCursos.find(x => x.id == id);
+    if(!duplicado){
+        listaCursos.push(cur);
+        return guardar();
+    }else{
+        return "El id del curso ingresa ya se encuentra en la base.";
+    }
+})
+
+const guardar = () => {
+    let datos = JSON.stringify(listaCursos);
+    fs.writeFile('ListadoCursos.json', datos, (err) => {
+        if(err) throw (err);
+        return "Registro guardado con exito";
+    })
+}
+
+hbs.registerHelper('Listar', () => {
+    listaCursos = require('../ListadoCursos.json');
+    let texto = '<table> \
+                        <th> Id </th> \
+                        <th> Nombre </th> \
+                        <th> Descripcion </th> \
+                        <th> Valor </th> \
+                        <th> Modalidad </th> \
+                        <th> Estado </th> \
+                    </thead> \
+                    <tbody>';
+    
+        listaCursos.forEach(element => {
+            if(element.id != undefined){
+                texto = texto + '<tr>' +
+                        "<td>" + element.id + '</td>' + 
+                        "<td>" + element.nombre + '</td>' + 
+                        "<td>" + element.descripcion + '</td>' + 
+                        "<td>" + element.valor + '</td>' +
+                        "<td>" + element.modalidad + '</td>' +
+                        "<td>" + element.estado + '</td>' +
+                        "</tr>"
+            }
+    });
+    texto = texto + "</tbody></table>";
+
+    return texto;
+})
--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
+const {check,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+require('./HelperCursos');
 
 const directorioPublico = path.join(__dirname, '../public');
 app.use(express.static(directorioPublico));
@@ -10,13 +13,32 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.set('view engine', 'hbs');
 
-app.post('/cursos', (req, res) => {
+app.get('/Cursos', (req, res) => {
     res.render('cursos', {
-        id: parseInt(req.body.id),
-        nombre: parseInt(req.body.nota1),
-        descripcion : parseInt(req.body.nota2),
-        valor : parseInt(req.body.nota3) 
-    })
+    });
+});
+
+app.post('/CrearCursos', 
+    [
+        check('id', 'El id es campo obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre es campo obligatorio').not().isEmpty(),
+        check('descripcion', 'La descripcion es campo obligatorio').not().isEmpty(),
+        check('valor', 'El valor es campo obligatorio').not().isEmpty(),
+    ],
+    (req, res) => {
+        
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.render('cursos', {error: errors.array()}); //res.status(422).jsonp(errors.array());
+        }else{
+                res.render('cursos', {
+                    id: parseInt(req.body.id),
+                    nombre: req.body.nombre,
+                    descripcion : req.body.descripcion,
+                    valor : parseInt(req.body.valor),
+                    modalidad: req.body.modalidad
+            })
+        }
 })
 
 app.listen(3000, () => {
